9ca5a69804bd930a6a5dde9089265131b676d55b
JMur2 <jakob.murray@marquette.edu>
2020-10-01 19:57:14 -0500

    Started locomotion file

--- locomotion/locomotion.py
+++ locomotion/locomotion.py
@@ -1,10 +1,32 @@
-# our first python file!
+"""
+This file is a test for the locomotion system. It is based on a demo file
+from the official ODrive github, which is linked below.  
+
+@author: Jake Murray
+@created: 9-25-2020
+
+Link to demo: https://github.com/madcowswe/ODrive/blob/master/tools/odrive_demo.py
+"""
+
 import odrive
+from odrive.enums import *
+import time
+import math
 
-odrv0 = odrive.find_any()
 
-axis = odrv0.axis0
-motor = axis.motor
+print("Finding an ODrive...\n")
+drive = odrive.find_any()
+
+# establishing variables for use.
+axis0 = drive.axis0
+axis1 = drive.axis1
+motor = axis0.motor
 encoder = axis.endcoder
 
-print(odrv0.vbus_voltage)
+print("Calibration...\n")
+axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+
+while axis0.current_state != AXIS_STATE_IDLE:
+    time.sleep(0.1)
+
+axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL    
