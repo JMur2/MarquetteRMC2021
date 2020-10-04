f6e4566232216a265a05c027eac7412fad0e394f
JMur2 <jakob.murray@marquette.edu>
2020-10-04 12:35:54 -0500

    Added new locomotion file

---
+++ locomotion/locomotion2.py
@@ -0,0 +1,12 @@
+"""
+The second locomotion test file for testing our robot's locomotion.
+Attempting to include keyboard operations to the motor's output, allowinf direct control over motion.
+
+@created: 10-4-2020
+"""
+
+def main():
+    
+    
+if __name__ == '__main__':
+    main()
