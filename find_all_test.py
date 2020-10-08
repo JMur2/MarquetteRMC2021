06edfc7aae040530c871ea66671a8bcf6c4ecbb5
JMur2 <jakob.murray@marquette.edu>
2020-10-07 21:32:39 -0500

    print out

--- find_all_test.py
+++ find_all_test.py
@@ -9,3 +9,6 @@ from pynput import keyboard
 if __name__ == '__main__':
     odrv0 = odrive.find_any(serial_number="206430804648")
     odrv1 = odrive.find_any(serial_number="20863880304E")
+    
+    print(odrv0.vbus_voltage)
+    print(odrv1.vbus_voltage)
