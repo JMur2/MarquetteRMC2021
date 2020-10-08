f01ed4248eed20e1a125e0e4f72a779bc08b2190
JMur2 <jakob.murray@marquette.edu>
2020-10-07 21:28:25 -0500

    testssssssss

--- find_all_test.py
+++ find_all_test.py
@@ -3,15 +3,8 @@ import time
 import math
 import py_compile
 
-# import ../odrive_ros/scr/odrive_ros/odrive_interface
-# import sys
-# import logging
-# import serial
-# from serial.serialutil import SerialException
-
 from odrive.enums import *
 from pynput import keyboard
-
 
 if __name__ == '__main__':
     stuff = list(odrive.find_all())
---
+++ locomotion/locomotion_digging_test.py
@@ -0,0 +1,86 @@
+import odrive
+import time
+import math
+import py_compile
+
+# import ../odrive_ros/scr/odrive_ros/odrive_interface
+# import sys
+# import logging
+# import serial
+# from serial.serialutil import SerialException
+
+from odrive.enums import *
+from pynput import keyboard
+
+
+def key_press(key):
+    # stop looking for key inputs 
+    if key == keyboard.Key.esc:
+        return False
+    
+    # look for key inputs
+    try:
+        # meant for single character keys - w, a, s, d etc.
+        k = key.char
+    except:
+        # meant for other keys - arrow keys, etc.
+        k = key.name
+    
+    # have commands for each key pressed
+    if k in ['w']:
+        # locomotion go forward
+        odrv0.axis0.controller.input_vel = -50
+        odrv0.axis1.controller.input_vel = 50
+        
+    elif k in ['a']:
+        # locomotion turn left
+        odrv0.axis0.controller.input_vel = 50
+        odrv0.axis1.controller.input_vel = 50
+        
+    elif k in ['s']:
+        # locomotion go backwards
+        odrv0.axis0.controller.input_vel = 50
+        odrv0.axis1.controller.input_vel = -50
+        
+    elif k in ['d']:
+        # locomotion turn right
+        odrv0.axis0.controller.input_vel = -50
+        odrv0.axis1.controller.input_vel = -50
+        
+    elif k in ['space']:
+        # stop all motors
+        odrv0.axis0.controller.input_vel = 0
+        odrv0.axis1.controller.input_vel = 0
+        odrv1.axis0.controller.input_vel = 0
+        
+    elif k in ['e']:
+        # print locomotion encoder errors 
+        odrv0.axis.encoder
+        
+    elif k in ['m']:
+        # print locomotion motor errors
+        odrv0.axis.motor
+        
+    elif k in ['i']:
+        # spin digging motor forward
+        odrv1.axis0.controller.input_vel = 10
+        
+    elif k in ['k']:
+        # spin digging motor backwards
+        odrv1.axis0.controller.input_vel = -10
+
+
+if __name__ == '__main__':
+    
+    if not odrive.find_all():
+        print("find all failed")
+    
+    stuff = list(odrive.find_all())
+    print(stuff)
+    
+    print("It is time to control the robot!\nThe controls are simple: wasd or the arrow keys move the robot directionally.")
+    print("Space will stop the robot in its tracks, and escape will end the control period altogether.")
+    listener = keyboard.Listener(on_press=key_press)
+    listener.start()
+    listener.join()
+    print("Ending program.")
