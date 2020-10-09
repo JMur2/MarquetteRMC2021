3766071663616eab237d2b674d2a5ac43a96b3f6
JMur2 <jakob.murray@marquette.edu>
2020-10-09 12:25:43 -0500

    ssh testing

--- find_all_test.py
+++ find_all_test.py
@@ -10,8 +10,9 @@ from odrive.enums import *
 from pynput import keyboard
 
 if __name__ == '__main__':
-    odrv0 = odrive.find_any(serial_number="206430804648")
-    odrv1 = odrive.find_any(serial_number="20863880304E")
+    print("This is an ssh test")
+#     odrv0 = odrive.find_any(serial_number="206430804648")
+#     odrv1 = odrive.find_any(serial_number="20863880304E")
     
-    print(odrv0.vbus_voltage)
-    print(odrv1.vbus_voltage)
+#     print(odrv0.vbus_voltage)
+#     print(odrv1.vbus_voltage)
