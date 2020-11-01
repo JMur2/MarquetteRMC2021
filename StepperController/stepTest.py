5397a188aee4d0b7c079a151eac021742212d510
Tony <akronohio1984@gmail.com>
2020-11-01 15:50:34 -0600

    New Test File

---
+++ StepperController/stepTest.py
@@ -0,0 +1,14 @@
+import subprocess
+import yaml
+ 
+def ticcmd(*args):
+  return subprocess.check_output(['ticcmd'] + list(args))
+ 
+status = yaml.load(ticcmd('-s', '--full'))
+ 
+position = status['Current position']
+print("Current position is {}.".format(position))
+ 
+new_target = -200 if position > 0 else 200
+print("Setting target position to {}.".format(new_target))
+ticcmd('--exit-safe-start', '--position', str(new_target))
