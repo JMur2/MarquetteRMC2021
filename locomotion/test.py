6b02fc8cc91d502fed2cec6a615962aca33ac3be
JMur2 <jakob.murray@marquette.edu>
2020-11-01 13:36:43 -0600

    New odrive test because the UDOO hates me

--- locomotion/test.py
+++ locomotion/test.py
@@ -1,2 +1,19 @@
+import odrive
+from odrive.enums import *
+import time
+import math
 
 if __name__ == "__main__":
+    print("Finding an odrive, this may take a few seconds...")
+    try:
+        odrv0 = odrive.find_any()
+    except:
+        print("Finding odrive failed")
+
+    print("Running Motor")
+    odrv0.axis0.controller.input_vel = 50
+
+    time.sleep(2)
+
+    print("Stopping Motor")
+    odrv0.axis0.controller.input_vel = 0
