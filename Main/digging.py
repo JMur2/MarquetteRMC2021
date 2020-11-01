bf0adc008fe065259cc0bdcd8c36d5d082756088
JMur2 <jakob.murray@marquette.edu>
2020-11-01 14:20:20 -0600

    created 3 subassembly files

---
+++ Main/digging.py
@@ -0,0 +1,12 @@
+"""
+This file houses all of the digging functionality
+
+@created: 11-1-2020
+"""
+
+
+
+class Digging:
+
+    def __init__(self):
+        # initializations
---
+++ Main/locomotion.py
@@ -0,0 +1,12 @@
+"""
+This file houses all of the locomotion based functionality.
+
+@created: 11-1-2020
+"""
+
+
+
+class Locomotion:
+
+    def __init__(self):
+        # initializations
