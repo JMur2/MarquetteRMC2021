43dec8fd55ebfd1e5e0a65bd28a174fda7fca755
JMur2 <jakob.murray@marquette.edu>
2020-11-01 13:40:35 -0600

    more new test changes

--- locomotion/test.py
+++ locomotion/test.py
@@ -2,6 +2,27 @@ import odrive
 from odrive.enums import *
 import time
 import math
+from pynput import keyboard
+
+
+def key_press(key):
+    # stop looking for key inputs 
+    if key == keyboard.Key.esc:
+        return False
+    
+    try:
+        # meant for single character keys - w, a, s, d etc.
+        k = key.char
+    except:
+        # meant for other keys - arrow keys, etc.
+        k = key.name
+    
+    if k in ['w']:
+        odrv0.axis0.controller.input_vel = 50
+    elif k in ['s']:
+        odrv0.axis0.controller.input_vel = -50
+    elif k in ['q']:
+        odrv0.axis0.controller.input_vel = 0
 
 if __name__ == "__main__":
     print("Finding an odrive, this may take a few seconds...")
@@ -9,11 +30,9 @@ if __name__ == "__main__":
         odrv0 = odrive.find_any()
     except:
         print("Finding odrive failed")
-
-    print("Running Motor")
-    odrv0.axis0.controller.input_vel = 50
-
-    time.sleep(2)
-
-    print("Stopping Motor")
-    odrv0.axis0.controller.input_vel = 0
+    
+    print("Ready to control")
+    listener = keyboard.Listener(on_press=key_press )
+    listener.start()
+    listener.join()
+    print("Ending program.")
