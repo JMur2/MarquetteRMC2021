4cf0f72b148f97ee892ef3145e1b7a1deb044e2d
JMur2 <jakob.murray@marquette.edu>
2020-11-18 22:05:19 -0600

    States and human comments, more common sense control setup

--- main/human.py
+++ main/human.py
@@ -3,7 +3,6 @@ This file contains all aspects of human control over the robot.
 
 @created: 10-25-2020
 """
-import odrive
 import time
 import math
 import sys
@@ -12,16 +11,22 @@ import termios
 
 from states import State
 from locomotion import Locomotion
-#from digging import digging_funcs
-#from dumping import dumping_funcs
-#from lidar import lidar_funcs
+from digging import Digging
+from dumping import Dumping
 
 class HumanControl:
 
+    #--------------------------------------------------------------------
+    # Initialize an instance of the human file for human control
+    #--------------------------------------------------------------------
     def __init__(self):
         # initializations
         print("init")
         
+    #--------------------------------------------------------------------
+    # Get a character from the keyboard
+    #  -- Uses termios, a library only available on Linux
+    #--------------------------------------------------------------------
     def get_char(self):
         file_descriptor = sys.stdin.fileno()
         old_settings = termios.tcgetattr(file_descriptor)
@@ -34,14 +39,28 @@ class HumanControl:
 
         return character
 
+    #--------------------------------------------------------------------
+    # Perform an action with the Robot based on the parameters
+    #
+    # param: curr_state -- the current contorl state of the robot
+    # param: char_val -- the ascii value of the keyboard input
+    #--------------------------------------------------------------------
     def control_robot(self, curr_state: str, char_val: int):
-        if   char_val == 119 and curr_state == "locomotion": # w -- forward
-            Locomotion.loco_forward(50)
-        elif char_val == 97  and curr_state == "locomotion": # a -- left
-            Locomotion.loco_left(50)
-        elif char_val == 115 and curr_state == "locomotion": # s -- back
-            Locomotion.loco_back(50)
-        elif char_val == 100 and curr_state == "locomotion": # d -- right
-            Locomotion.loco_back(50)
-        elif char_val == 120 and curr_state == "locomotion": # x -- stop
-            Locomotion.loco_stop()
+
+        if curr_state == "locomotion":
+            if   char_val == 119: # w -- forward
+                Locomotion.loco_forward(50)
+            elif char_val == 97 : # a -- left
+                Locomotion.loco_left(50)
+            elif char_val == 115: # s -- back
+                Locomotion.loco_back(50)
+            elif char_val == 100: # d -- right
+                Locomotion.loco_back(50)
+            elif char_val == 120: # x -- stop
+                Locomotion.loco_stop()
+        elif curr_state == "digging":
+            print("digging functions and keys here")
+        elif curr_state == "dumping":
+            print("dumping functions and heys here")
+        else:
+            print("unrecognized control state, please change this and try again")
--- main/states.py
+++ main/states.py
@@ -7,18 +7,38 @@ Will allow for adjustments of the state, and will restrict certain actions depen
 
 class State:
     
+    #--------------------------------------------------------------------
+    # Get the current control state
+    #
+    # Possible states: locomotion, digging, dumping, startup
+    #--------------------------------------------------------------------
     @classmethod
     def get_control_state(cls) -> str:
         return cls.control_state
     
+    #--------------------------------------------------------------------
+    # Set the current control state
+    #
+    # Possible states: locomotion, digging, dumping, startup
+    #--------------------------------------------------------------------
     @classmethod
     def set_control_state(cls, new_state: str):
         cls.control_state = new_state
     
+    #--------------------------------------------------------------------
+    # Get the current operation state
+    #
+    # Possible states: human, lidar
+    #--------------------------------------------------------------------
     @classmethod
     def get_operation_state(cls) -> str:
         return cls.operation_state
 
+    #--------------------------------------------------------------------
+    # Set the current operation state
+    #
+    # Possible states: human, lidar
+    #--------------------------------------------------------------------
     @classmethod
     def set_operation_state(cls, new_state: str):
         cls.operation_state = new_state
