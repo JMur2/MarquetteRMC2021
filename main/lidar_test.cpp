22f4c660fa560985a4c396ecd4302dc7a0566588
JMur2 <jakob.murray@marquette.edu>
2020-12-09 18:45:44 -0600

    started CPP testing file

---
+++ main/lidar_test.cpp
@@ -0,0 +1,9 @@
+#include <sick_safetyscanners_base/SickSafetyscanners.h>
+#include <sick_safetyscanners_base/Exceptions.h>
+#include <sick_safetyscanners_base/Types.h>
+#include <sick_safetyscanners_base/datastructure/CommSettings.h>
+
+int main() {
+
+    
+}
