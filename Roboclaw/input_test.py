745990a0e07f88fbb665b0acfd7d268f4b8e4c59
JMur2 <jakob.murray@marquette.edu>
2021-03-08 19:31:50 -0600

    bringing back input

--- Roboclaw/input_test.py
+++ Roboclaw/input_test.py
@@ -27,25 +27,23 @@ if __name__ == '__main__':
     roboclaw.Open()
     print("Robot is ready to control, use WASD to control direction and Q to quit")
 
-    roboclaw.ForwardM1(128, 127)
-
-#     try:
-#         while True:
-#             char = get_char()
-#             char_val = ord(char)
+    try:
+        while True:
+            char = get_char()
+            char_val = ord(char)
             
-#             if char.lower() == "q":
-#                 break
-#             elif char.lower() == "w":
-#                 roboclaw.ForwardM1(0x80, 127)
-#                 #roboclaw.ForwardM2(0x80, 127)
-#             elif char.lower() == "s":
-#                 roboclaw.BackwardM1(0x80, 127)
-#                 #roboclaw.BackwardM2(0x80, 127)
-# #            elif char.lower() == "x":
-# #                roboclaw.ForwardM1(0x80, 0)
-#     finally:
-#         roboclaw.ForwardM1(0x80, 0)
-#         #roboclaw.ForwardM2(0x80, 0)
-#         print("------")
+            if char.lower() == "q":
+                break
+            elif char.lower() == "w":
+                roboclaw.ForwardM1(0x80, 127)
+                #roboclaw.ForwardM2(0x80, 127)
+            elif char.lower() == "s":
+                roboclaw.BackwardM1(0x80, 127)
+                #roboclaw.BackwardM2(0x80, 127)
+#            elif char.lower() == "x":
+#                roboclaw.ForwardM1(0x80, 0)
+    finally:
+        roboclaw.ForwardM1(0x80, 0)
+        #roboclaw.ForwardM2(0x80, 0)
+        print("------")
     print("Program ending")
