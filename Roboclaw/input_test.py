fe907a4972c0fd57abc300f328e004d9fa16c848
JMur2 <jakob.murray@marquette.edu>
2021-03-08 19:17:14 -0600

    back to 0

--- Roboclaw/input_test.py
+++ Roboclaw/input_test.py
@@ -23,7 +23,7 @@ def get_char():
 
 if __name__ == '__main__':
     print("Searching for roboclaw, this may take a few seconds...\n")
-    roboclaw = Roboclaw("/dev/ttyACM1", 38400)
+    roboclaw = Roboclaw("/dev/ttyACM0", 38400)
     roboclaw.Open()
     print("Robot is ready to control, use WASD to control direction and Q to quit")
     try:
@@ -35,14 +35,14 @@ if __name__ == '__main__':
                 break
             elif char.lower() == "w":
                 roboclaw.ForwardM1(0x80, 127)
-                roboclaw.ForwardM2(0x80, 127)
+                #roboclaw.ForwardM2(0x80, 127)
             elif char.lower() == "s":
                 roboclaw.BackwardM1(0x80, 127)
-                roboclaw.BackwardM2(0x80, 127)
+                #roboclaw.BackwardM2(0x80, 127)
 #            elif char.lower() == "x":
 #                roboclaw.ForwardM1(0x80, 0)
     finally:
         roboclaw.ForwardM1(0x80, 0)
-        roboclaw.ForwardM2(0x80, 0)
+        #roboclaw.ForwardM2(0x80, 0)
         print("------")
     print("Program ending")
