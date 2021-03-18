f37e36ed617a3171743a15b848593ddc3cd5cc7d
JMur2 <jakob.murray@marquette.edu>
2021-03-18 18:19:19 -0500

    position change

--- StepperController/stepTest.py
+++ StepperController/stepTest.py
@@ -53,6 +53,7 @@ status = yaml.load(ticcmd('-s', '--full'))
 position = status['Current position']
 print("Current position is {}.".format(position))
 
-new_target = -200 if position > 0 else 200
+#new_target = -200 if position > 0 else 200
+new_target = -220
 print("Setting target position to {}.".format(new_target))
 ticcmd('--exit-safe-start', '-d', '00320100', '--position', str(new_target))
