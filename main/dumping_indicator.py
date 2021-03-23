ae084ad6235b3907d77096e53fc99cc2cdfc723d
Albertooo0 <47334796+Albertooo0@users.noreply.github.com>
2021-03-22 19:20:09 -0500

    added the functions for the dumping indicators

--- main/dumping_indicator.py
+++ main/dumping_indicator.py
@@ -1,6 +1,27 @@
 
 class dumpingIndicator:
+
     def __init__(self):
 
         #check if the steps of dumping automation are ready
-        
+        self.readyExtend = True
+        self.readyDump = False
+        self.readyRetract = False
+
+    def SetExtend(self):
+        self.readyExtend =  True
+
+    def SetDump(self):
+        self.readyDump = True
+    
+    def SetRetract(self):
+        self.readyRetract = True 
+    
+    def endExtend(self):
+        self.readyExtend = False
+    
+    def endDump(self):
+        self.readyDump = False
+
+    def endRetract(self):
+        self.readyRetract = False
