b0db1404198fba6a9ef89889bbf1d5747133d8dc
JMur2 <jakob.murray@marquette.edu>
2021-05-02 15:19:19 -0500

    clean up

--- lidar/lidar_test.cpp
+++
@@ -1,9 +0,0 @@
-#include <sick_safetyscanners_base/SickSafetyscanners.h>
-#include <sick_safetyscanners_base/Exceptions.h>
-#include <sick_safetyscanners_base/Types.h>
-#include <sick_safetyscanners_base/datastructure/CommSettings.h>
-
-int main() {
-
-    
-}
